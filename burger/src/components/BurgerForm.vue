9ff67d5a5f2b15e30074e0ccea0cd17ee534e050
kewen <kewensilva58@gmail.com>
2022-08-25 22:31:02 -0300

    post

--- burger/src/components/BurgerForm.vue
+++ burger/src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de mensagem</p>
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="nome">Nome do cliente:</label>
           <input
@@ -59,7 +59,6 @@ export default {
       pao: null,
       carne: null,
       opcionais: [],
-      status: 'Solicitado',
       msg: null
     };
   },
@@ -71,8 +70,21 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
+    },
+    async createBurger(e){
+      e.preventDefault();
+
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado"
+      }
+      console.log(data)
     }
   },
+
   mounted() {
     this.getIngredientes()
   }
