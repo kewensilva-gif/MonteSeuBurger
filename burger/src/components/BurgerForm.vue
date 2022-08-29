47c84e6119fe12ca567114fc494e36785991d81b
Kéwen Silva <kewensilva58@gmail.com>
2022-08-28 21:36:36 -0300

    mensagem
    exibir mensagem de pedido enviado

--- burger/src/components/BurgerForm.vue
+++ burger/src/components/BurgerForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Componente de mensagem</p>
+    <Message :msg="msg" v-show="msg"/>
     <div>
       <form id="burger-form" @submit="createBurger">
         <div class="input-container">
@@ -48,8 +48,12 @@
 </template>
 
 <script>
+import Message from './Message.vue';
 export default {
   name: "BurgerForm",
+  components:{
+    Message
+  },
   data() {
     return {
       paes: null,
@@ -90,7 +94,14 @@ export default {
       });
       const res = await req.json();
       
-      
+      // Colocar uma msg de sistema
+      this.msg = `Pedido Nº ${res.id} realizado com sucesso`
+
+      // Remover mensagem
+      setTimeout(() => {
+        this.msg = ""
+      }, 3000);
+
       // Limpar os campos
       this.nome = "";
       this.carne = "";
